26f72b8dd743e2b92147cc9acace1094730e9d86
codecademy <ccuser@codecademy.com>
2021-07-29 08:18:45 +0000

    completed the portfolio project

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
   root 'pages#welcome'
+    get 'welcome' => 'pages#welcome'
     get 'portfolio' => 'portfolio#portfolio'
     get 'about' => 'about#about'
+    get 'contact' => 'contact#contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
